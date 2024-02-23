14562034af9eb4da745b85414445479ee2a398d6
Evander-byte <emondragon571@gmail.com>
2024-02-23 15:50:10 -0600

    Estilos generales y campos de formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,90 @@
 
 const Formulario = () => {
   return (
-    <div>
-      <h2>Hola desde formulario</h2>
+    <div className="md:w-1/2 lg:w -2/5">
+      <h2  className="font-black text-3xl text-center">
+        Seguimiento Pacientes
+      </h2>
+      <p className="text-lg mt-5 text-center">Añade Pacientes y  {""}<span className="text-indigo-600 font-bold">Administralos </span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div className="mb-5">
+          <label
+            htmlFor="nombre"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota: 
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+            type="text"
+            placeholder="Nombre de la mascota"
+            id="nombre"
+          />
+        </div> 
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Propietario: 
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+            type="text"
+            placeholder="Nombre del Propietario"
+            id="propietario"
+          />
+        </div> 
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email: 
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+            type="email"
+            placeholder="Email del propietario"
+            id="email"
+          />
+        </div> 
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Alta: 
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+            type="date"
+            id="alta"
+          />
+        </div> 
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Sintomas: 
+          </label>
+          <textarea 
+            id="sintomas" 
+            cols="30" 
+            rows="10"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+            placeholder="Describe los sintomas de la mascota"
+          />
+        </div> 
+        <input 
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
+          value="Agregar Mascota"
+        />
+      </form>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold uppercase">Hola desde Header</h1>
-    </div>
+    <>
+      <h1 className = "font-black text-5xl text-center md:w-2/3 mx-auto">
+        Seguimiento Pacientes {""}
+        <span className = "text-indigo-600">Veterinaria</span>
+      </h1>
+    </>
   )
 }
 
