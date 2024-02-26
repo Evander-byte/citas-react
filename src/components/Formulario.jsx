0e8564588141026d7a2bb7824f270a46e05d2f83
Evander-byte <emondragon571@gmail.com>
2024-02-26 14:15:51 -0600

    Funcionalidad de props, ListadoPacientes y validaciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,60 @@
+import { useState } from "react"
 
 
-const Formulario = () => {
+const Formulario = ({ pacientes,setPacientes }) => {
+
+
+  const [nombre, setNombre] = useState('')
+  const [propietario, setPropietario] = useState('')
+  const [email, setEmail] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [sintomas, setSintomas] = useState('')
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if([nombre, propietario, email, fecha, sintomas].includes('')){
+      console.log('Los campos no deben ir vacios')
+      setError(true)
+      return
+    }
+
+
+    // Objeto de paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas
+    }
+
+    setPacientes([...pacientes, objetoPaciente])
+    
+    setNombre('')
+    setEmail('')
+    setPropietario('')
+    setSintomas('')
+    setError(false)
+
+  }
+  
+
   return (
-    <div className="md:w-1/2 lg:w -2/5">
+    <div className=" mx-5 md:w-1/2 lg:w-2/5">
       <h2  className="font-black text-3xl text-center">
         Seguimiento Pacientes
       </h2>
-      <p className="text-lg mt-5 text-center">Añade Pacientes y  {""}<span className="text-indigo-600 font-bold">Administralos </span>
+      <p className="text-lg mt-5 text-center mb-10">Añade Pacientes y  {""}<span className="text-indigo-600 font-bold">Administralos </span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        {error && 
+          <div className="bg-red-800 rounded-xl p-3 text-center mb-3 uppercase font-bold text-white">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        }
         <div className="mb-5">
           <label
             htmlFor="nombre"
@@ -22,6 +67,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre de la mascota"
             id="nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div> 
         <div className="mb-5">
@@ -36,6 +83,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre del Propietario"
             id="propietario"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div> 
         <div className="mb-5">
@@ -50,6 +99,8 @@ const Formulario = () => {
             type="email"
             placeholder="Email del propietario"
             id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div> 
         <div className="mb-5">
@@ -57,12 +108,14 @@ const Formulario = () => {
             htmlFor="alta"
             className="block text-gray-700 uppercase font-bold"
           >
-            Alta: 
+            Ingreso: 
           </label>
           <input
             className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
             type="date"
             id="alta"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div> 
         <div className="mb-5">
@@ -78,6 +131,8 @@ const Formulario = () => {
             rows="10"
             className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
             placeholder="Describe los sintomas de la mascota"
+            value={sintomas}
+            onChange={(e) => setSintomas (e.target.value)}
           />
         </div> 
         <input 
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,28 @@
+
+
+const Paciente = () => {
+  return (
+    <div className="mx-5 my-10  bg-white shadow-md rounded-xl p-5 py-5">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case ">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case ">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+          <span className="font-normal normal-case ">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+          <span className="font-normal normal-case ">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Ingreso: {''}
+          <span className="font-normal normal-case ">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+          <span className="font-normal normal-case ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque doloremque ipsum maiores officia veniam possimus similique ipsa tenetur rem asperiores porro, cum et saepe consequuntur natus? Corporis assumenda reprehenderit aspernatur!</span>
+        </p>
+      </div>
+  )
+}
+
+export default Paciente
