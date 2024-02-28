42386098c780ddc82e7a029047aecc595f1812c6
Evander-byte <emondragon571@gmail.com>
2024-02-28 12:06:41 -0600

    Inyectanod datos en el paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,25 +1,24 @@
 
 
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+  
+  const {nombre, propietario, email, fecha, sintomas} = paciente
   return (
     <div className="mx-5 my-10  bg-white shadow-md rounded-xl p-5 py-5">
         <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
-          <span className="font-normal normal-case ">Hook</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
-          <span className="font-normal normal-case ">Hook</span>
+          <span className="font-normal normal-case ">{nombre}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
-          <span className="font-normal normal-case ">Hook</span>
+          <span className="font-normal normal-case ">{propietario}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
-          <span className="font-normal normal-case ">Hook</span>
+          <span className="font-normal normal-case ">{email}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Ingreso: {''}
-          <span className="font-normal normal-case ">Hook</span>
+          <span className="font-normal normal-case ">{fecha}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
-          <span className="font-normal normal-case ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque doloremque ipsum maiores officia veniam possimus similique ipsa tenetur rem asperiores porro, cum et saepe consequuntur natus? Corporis assumenda reprehenderit aspernatur!</span>
+          <span className="font-normal normal-case ">{sintomas}</span>
         </p>
       </div>
   )
